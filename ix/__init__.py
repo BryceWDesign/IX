a746be1a102bb3ab038c16683cacad1100f1a6ed
Bryce Lovell <40742705+BryceWDesign@users.noreply.github.com>
2026-05-22 20:25:18 -0700

    Update __init__.py

--- ix/__init__.py
+++ ix/__init__.py
@@ -3,18 +3,25 @@
 from .ast import Program
 from .errors import IXError, IXSyntaxError, IXValidationError
 from .parser import IXParser, parse_ix
+from .runtime import ExecutionResult, IXRuntime, IXRuntimeError, run_ix
+from .tracing import TraceEvent
 from .validator import Diagnostic, IXValidator, validate_ix
 from .version import __version__
 
 __all__ = [
     "Diagnostic",
+    "ExecutionResult",
     "IXError",
     "IXParser",
+    "IXRuntime",
+    "IXRuntimeError",
     "IXSyntaxError",
     "IXValidationError",
     "IXValidator",
     "Program",
+    "TraceEvent",
     "__version__",
     "parse_ix",
+    "run_ix",
     "validate_ix",
 ]
