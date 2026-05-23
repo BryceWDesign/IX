bc7960cd7a1c52a0b7525fa9ef0de59ee6f6cf93
Bryce Lovell <40742705+BryceWDesign@users.noreply.github.com>
2026-05-22 20:23:32 -0700

    Update __init__.py

--- ix/__init__.py
+++ ix/__init__.py
@@ -1,5 +1,20 @@
 """IX public package surface."""
 
+from .ast import Program
+from .errors import IXError, IXSyntaxError, IXValidationError
+from .parser import IXParser, parse_ix
+from .validator import Diagnostic, IXValidator, validate_ix
 from .version import __version__
 
-__all__ = ["__version__"]
+__all__ = [
+    "Diagnostic",
+    "IXError",
+    "IXParser",
+    "IXSyntaxError",
+    "IXValidationError",
+    "IXValidator",
+    "Program",
+    "__version__",
+    "parse_ix",
+    "validate_ix",
+]
