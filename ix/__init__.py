7956461b357677fd87fdb6e67a66e87a76e57f9e
Bryce Lovell <40742705+BryceWDesign@users.noreply.github.com>
2026-05-22 20:32:31 -0700

    Update __init__.py

--- ix/__init__.py
+++ ix/__init__.py
@@ -1,15 +1,17 @@
 """IX public package surface."""
 
-from .ast import Program
+from .ast import Program, ToolArgument, ToolCallStatement
 from .errors import IXError, IXSyntaxError, IXValidationError
 from .formatting import IXFormatError, IXFormatter, format_ix
 from .parser import IXParser, parse_ix
 from .runtime import ExecutionResult, IXRuntime, IXRuntimeError, run_ix
+from .tools import BuiltInToolRegistry, IXToolError, ToolSpec
 from .tracing import TraceEvent
 from .validator import Diagnostic, IXValidator, validate_ix
 from .version import __version__
 
 __all__ = [
+    "BuiltInToolRegistry",
     "Diagnostic",
     "ExecutionResult",
     "IXError",
@@ -19,9 +21,13 @@ __all__ = [
     "IXRuntime",
     "IXRuntimeError",
     "IXSyntaxError",
+    "IXToolError",
     "IXValidationError",
     "IXValidator",
     "Program",
+    "ToolArgument",
+    "ToolCallStatement",
+    "ToolSpec",
     "TraceEvent",
     "__version__",
     "format_ix",
